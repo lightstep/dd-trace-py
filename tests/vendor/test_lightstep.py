20a9857d60a36019c4708fca80567e0e66bfc438
Alex Boten <aboten@lightstep.com>
2020-03-20 15:29:41 -0700

    fix test

--- tests/vendor/test_lightstep.py
+++ tests/vendor/test_lightstep.py
@@ -5,8 +5,8 @@ from ..base import BaseTestCase
 class LightstepRunTests(BaseTestCase):
     def test_priority_sampling_from_env(self):
         """
-        LIGHTSTEP_METRICS_DISABLE disables runtime metrics
+        LS_METRICS_ENABLED disables runtime metrics
         """
-        with self.override_env(dict(LIGHTSTEP_METRICS_DISABLE="True")):
+        with self.override_env(dict(LS_METRICS_ENABLED="False")):
             out = subprocess.check_output(["ls-trace-run", "python", "tests/vendor/lstrace_run_disable_metrics.py"])
             assert out.startswith(b"Test success")
