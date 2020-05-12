acc300227cec99ba42c1feccf1eb77ffc8a73187
Julien Danjou <julien@danjou.info>
2020-05-12 15:16:41 +0200

    Upgrade flake8 to 3.8 + black ddtrace{,.span} (#1429)
    * chore: upgrade flake8 to 3.8 and fix new warnings
    
    * chore(black): black ddtrace and ddtrace.span

--- ddtrace/__init__.py
+++ ddtrace/__init__.py
@@ -5,11 +5,12 @@ from .internal.import_hooks import patch as patch_import_hooks
 
 patch_import_hooks()  # noqa: E402
 
-from .monkey import patch, patch_all
-from .pin import Pin
-from .span import Span
-from .tracer import Tracer
-from .settings import config
+from .monkey import patch, patch_all  # noqa: E402
+from .pin import Pin  # noqa: E402
+from .span import Span  # noqa: E402
+from .tracer import Tracer  # noqa: E402
+from .settings import config  # noqa: E402
+from .utils.deprecation import deprecated  # noqa: E402
 
 
 __version__ = "0.2.0"
@@ -38,6 +39,7 @@ def _excepthook(tp, value, traceback):
         return _ORIGINAL_EXCEPTHOOK(tp, value, traceback)
 
 
+@deprecated("This method will be removed altogether", "1.0.0")
 def install_excepthook():
     """Install a hook that intercepts unhandled exception and send metrics about them."""
     global _ORIGINAL_EXCEPTHOOK
@@ -45,6 +47,7 @@ def install_excepthook():
     sys.excepthook = _excepthook
 
 
+@deprecated("This method will be removed altogether", "1.0.0")
 def uninstall_excepthook():
     """Uninstall the global tracer except hook."""
     sys.excepthook = _ORIGINAL_EXCEPTHOOK
